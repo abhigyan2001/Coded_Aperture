dd4c53518b4309750a69f61c7aeeffd8ef0e039c
abhigyan2001 <rookhallow31143@gmail.com>
2023-01-09 19:57:54 +0530

    Made changes

--- tensor_reader.py
+++ tensor_reader.py
@@ -1,2 +1,13 @@
 import numpy as np
-import 
+import cv2
+import torch
+import os
+
+datasets = ['BarcaArchVis','Classroom','Cupcakes','RippleDreams','Splash']
+dnames = ['vis_','Classroom_','Cupcakes_','bubbly_','Splash33_']
+for dataset,dname in zip(datasets,dnames):
+    files = os.listdir(dataset)
+    for f in files:
+        if dname == f[:len(dname)]:
+            file = os.path.join(dataset,f)
+            img = cv2.imread(file)
